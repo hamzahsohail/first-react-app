69e2b1d824c89cc5c6fcb635329c4e1ce505c3b5
Hamzah <hamzahsohail@yahoo.com>
2024-09-09 00:05:21 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import "./App.css";
+
+function App() {
+  return <>
+  <nav>
+    <li>HOME</li>
+    <li>ABOUT</li>
+    <li>CONTACT</li>
+  </nav>
+  </>;
+}
+
+export default App;
