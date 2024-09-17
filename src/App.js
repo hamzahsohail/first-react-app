ab2202d17f47e0336a2ffce7ca437611b6867b03
Hamzah <hamzahsohail@yahoo.com>
2024-09-17 22:21:11 +0500

    dated 170924

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
+import React, { useState } from "react";
 import Textform from "./components/Textform";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 
 function App() {
-  let darkStyle = {
+  const [darkStyle, setDarkStyle] = useState({
     color: "white",
     backgroundColor: "black",
-  };
+  });
 
   return (
     <>
       <div style={darkStyle}>
         <Navbar title="TEXT-UTILS" />
         <div className="container">
-          <Textform heading1="Text Form" heading2="Text Summary"/>
+          <Textform heading1="Text Form" heading2="Text Summary" />
           <About />
         </div>
       </div>
